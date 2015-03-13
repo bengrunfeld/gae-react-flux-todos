5457b32b8c20284abe67d1a4473b4532fe36e117
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-03-12 17:26:49 -0700

    Reorder components by clicking button

--- src/js/components/todo-list.js
+++ src/js/components/todo-list.js
@@ -3,6 +3,12 @@ var React = require('react');
 var Todo = require('./todo');
 
 var TodoList = React.createClass({
+  reorder: function(e) {
+    a = this.props.data[0];
+    this.props.data.splice(0, 1);
+    this.props.data.splice(2, 0, a);
+    this.setState(this.props.data);
+  },
   render:function(){
     // Avoid error from rendering if props is empty
     // TODO: Should probably take care of this in the parent component
@@ -20,9 +26,10 @@ var TodoList = React.createClass({
       );
     });
     return (
-      <form className="todoList">
+      <div className="todoList">
         {todoNodes}
-      </form>
+        <button onClick={this.reorder}>Click me</button>
+      </div>
     )
   }
 });
